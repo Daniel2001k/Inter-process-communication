33b79394eeda8457d8a1c97dd093e72bc3baffed
Daniel2001k <68070950+Daniel2001k@users.noreply.github.com>
2024-04-19 23:47:55 +0200

    Update server.c

--- Bez obslugi bledow/FIFO/server.c
+++ Bez obslugi bledow/FIFO/server.c
@@ -1,54 +1,60 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <signal.h>
+
+#include <math.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
 
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#include <fcntl.h>
-#include <math.h>
-#include <unistd.h>
- 
 int file;
- 
-void signalHandler(int sig) 
+
+void al(int a)
 {
-        printf("\nserver stop\n");
+        printf("koniec pracy serwera");
         close(file);
         unlink("/tmp/fifo");
         exit(0);
 }
- 
-int main() {
-        signal(SIGINT, signalHandler);
+
+int main()
+{
+        signal(SIGTERM, al);
+        signal(SIGINT, al);
 
         mkfifo("/tmp/fifo", 0600 | O_CREAT);
- 
-        double x;
-        long d;
-        double xPowed;
+
+        double a;
+        long b;
+
+        double power;
         double result;
 
-        while(1) 
+        for(;;)
         {
                 file = open("/tmp/fifo", O_RDWR);
- 
-                read(file, &x, sizeof(double));
-                read(file, &d, sizeof(double));
- 
-                printf("Liczba x: %f\nLiczba d: %ld\n", x, d);
-                xPowed = pow(x, d);
-                printf("Liczba x do potegi d: %f\n", xPowed);
- 
-                write(file, &xPowed, sizeof(double));
 
-                sleep(3);
- 
-                read(file, &result, sizeof(double));
-                
-                printf("Wynik: %f\n", result);
- 
-                close(file);
+                read(file, &a, sizeof(double));
+                read(file, &b, sizeof(long));
+
+                printf("a: %lf, b: %ld\n", a, b);
+                power = pow(a, b);
+                printf("%lf^%f=%lf\n", a, b, power);
+
+                write(file, &power, sizeof(double));
+
                 sleep(1);
+
+                read(file, &result, sizeof(double));
+
+                printf("wynik=%lf\n", result);
+
+                close(file);
         }
+
+        return 0;
 }
