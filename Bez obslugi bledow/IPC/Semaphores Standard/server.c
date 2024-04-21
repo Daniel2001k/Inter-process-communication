ebd6cb98ca58f3ce2227244c158e9185e2faac31
Daniel2001k <68070950+Daniel2001k@users.noreply.github.com>
2024-04-21 21:01:48 +0200

    Update server.c

--- Bez obslugi bledow/IPC/Semaphores Standard/server.c
+++ Bez obslugi bledow/IPC/Semaphores Standard/server.c
@@ -1,52 +1,59 @@
-#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-#include <sys/types.h>
-#include <sys/shm.h>
-#include <sys/sem.h>
-#include <sys/ipc.h>
+#include <string.h>
+#include <signal.h>
 
 #include <fcntl.h>
+#include <errno.h>
 #include <unistd.h>
+#include <math.h>
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/types.h>
 
 #define SHM_SIZE 1024
 #define SHM_KEY 1234
 
 int main()
 {
-    for (;;)
-    {
-        int shmid = shmget(SHM_KEY, SHM_SIZE, IPC_CREAT | 0666);
-        char *shmaddr = shmat(shmid, (void *)0, 0);
-        int semid = semget(SHM_KEY, 1, IPC_CREAT | 0666);
-
-        struct sembuf sem_st = {0, 0, 0};
-
-        semop(semid, &sem_st, 1);
-
-        struct sembuf sem_wait = {0, -1, 0};
-
-        semop(semid, &sem_wait, 1);
-
-        printf("wiadomosc %s", shmaddr);
-
-        char *wiadomosc = strtok(shmaddr, ",");
-        char wiadomosci[3][100];
-        int i = 0;
-        while (wiadomosc != NULL && i < 3)
+        for(;;)
         {
-            strcpy(wiadomosci[i], wiadomosc);
-            i++;
-            wiadomosc = strtok(NULL, ",");
+                int shmid = shmget(SHM_KEY, SHM_SIZE, IPC_CREAT | 0666);
+                char *shmaddr = shmat(shmid, (void*)0, 0);
+                int semid = semget(SHM_KEY, 1, IPC_CREAT | 0666);
+
+                struct sembuf sem_st = {0, 0, 0};
+
+                semop(semid, &sem_st, 1);
+
+                sem_st.sem_op = -1;
+
+                semop(semid, &sem_st, 1);
+
+                printf("Wiadomosc: %s\n", shmaddr);
+
+                char ws[100];
+                double wd1;
+                double wd2;
+
+                sscanf(shmaddr, "%s %lf %lf", ws, &wd1, &wd2);
+
+                sprintf(shmaddr, "Serwer: %s - %lf", ws, wd1 * wd2);
+
+                sem_st.sem_op = 1;
+
+                semop(semid, &sem_st, 1);
+
+                sem_st.sem_op = -1;
+                semop(semid, &sem_st, 1);
+                printf("%s\n", shmaddr);
+
+                shmdt(shmaddr);
+                shmctl(shmid, IPC_RMID, NULL);
+                semctl(semid, 0, IPC_RMID);
         }
-        sprintf(shmaddr, "%s + %lf", wiadomosci[0], atof(wiadomosci[1]) * atof(wiadomosci[2]));
 
-        struct sembuf sem_send = {0, 1, 0};
-
-        semop(semid, &sem_send, 1);
-        shmdt(shmaddr);
-        semctl(semid, 0, IPC_RMID);
-        shmctl(shmid, IPC_RMID, NULL);
-    }
+        return 0;
 }
