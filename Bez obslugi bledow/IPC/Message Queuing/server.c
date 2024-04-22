220b0c9ec9a7230742d65260273ed5719dedf046
Daniel2001k <68070950+Daniel2001k@users.noreply.github.com>
2024-04-22 18:34:44 +0200

    Update server.c

--- Bez obslugi bledow/IPC/Message Queuing/server.c
+++ Bez obslugi bledow/IPC/Message Queuing/server.c
@@ -4,41 +4,41 @@
 #include <signal.h>
 
 #include <fcntl.h>
-#include <unistd.h>
-#include <math.h>
 #include <errno.h>
+#include <math.h>
+#include <unistd.h>
 
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
+typedef struct
+{
+        long mtype;
+        double msg;
+} messaged;
+
+typedef struct
+{
+        long mtype;
+        char msg[100];
+} messages;
+
 char path[] = "/tmp/kolejki";
-int queue_id;
+int qid;
 
-typedef struct mystring
+messaged* msgd;
+messages* msgs;
+
+void al(int a)
 {
-        long mtype;
-        char text[1024];
-} message_s;
+        printf("Koniec pracy serwera\n");
 
-typedef struct mydouble
-{
-        long mtype;
-        double number;
-} message_d;
+        msgctl(qid, IPC_RMID, NULL);
 
-message_s* msg_s;
-message_d* msg_d;
-
-void al()
-{
-        msgctl(queue_id, IPC_RMID, NULL);
-
-        free(msg_s);
-        free(msg_d);
+        free(msgd);
+        free(msgs);
 
         unlink(path);
-
-        printf("Koniec pracy serwera");
 
         exit(0);
 }
@@ -48,43 +48,45 @@ int main()
         signal(SIGTERM, al);
         signal(SIGINT, al);
 
-        size_t msg_s_size = sizeof(message_s) - sizeof(long);
-        size_t msg_d_size = sizeof(message_d) - sizeof(long);
+
+        size_t msgs_size = sizeof(messages) - sizeof(long);
+        size_t msgd_size = sizeof(messaged) - sizeof(long);
+        msgs = malloc(sizeof(messages));
+        msgd = malloc(sizeof(messaged));
+
 
         int file = open(path, O_EXCL | O_CREAT | 0666);
-
         if (access(path, F_OK) == -1)
         {
                 perror("Brak dostepu do pliku");
                 unlink(path);
                 return -1;
         }
-
         close(file);
 
+
         key_t key = ftok(path, 0);
+        qid = msgget(key, O_EXCL | IPC_CREAT | 0666);
 
-        msg_s = malloc(sizeof(message_s));
-        msg_d = malloc(sizeof(message_d));
-
-        queue_id = msgget(key, O_EXCL | IPC_CREAT | 0666);
-
-        for(;;)
+        for (;;)
         {
-                msgrcv(queue_id, (void*)msg_s, msg_s_size, 1, 0);
-                msgrcv(queue_id, (void*)msg_d, msg_d_size, 2, 0);
+                msgrcv(qid, (void*)msgs, msgs_size, 1, 0);
+                msgrcv(qid, (void*)msgd, msgd_size, 1, 0);
 
-                msg_s->mtype = 3;
-                sprintf(msg_s->text, "%s %lf", msg_s->text, msg_d->number);
 
-                msg_d->mtype = 3;
-                msg_d->number = pow(msg_d->number, 2);
+                msgs->mtype = 2;
+                msgd->mtype = 2;
 
-                msgsnd(queue_id, (void*)msg_s, msg_s_size, 0);
-                msgsnd(queue_id, (void*)msg_d, msg_d_size, 0);
+                sprintf(msgs->msg, "%s %lf", msgs->msg, msgd->msg);
+                msgd->msg = pow(msgd->msg, 2);
 
-                msgrcv(queue_id, (void*)msg_s, msg_s_size, 4, 0);
-                printf("Odebralem wiadomosc: %s\n", msg_s->text);
+
+                msgsnd(qid, (void*)msgs, msgs_size, 0);
+                msgsnd(qid, (void*)msgd, msgd_size, 0);
+
+
+                msgrcv(qid, (void*)msgs, msgs_size, 3, 0);
+                printf("Wynik: %s\n", msgs->msg);
         }
 
         return 0;
