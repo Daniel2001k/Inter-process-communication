bc1557cd3550fab944b757d3ea98ff59a9d5df30
Daniel2001k <68070950+Daniel2001k@users.noreply.github.com>
2024-04-22 20:01:31 +0200

    Update server.c

--- Bez obslugi bledow/IPC/Semaphores Standard/server.c
+++ Bez obslugi bledow/IPC/Semaphores Standard/server.c
@@ -4,47 +4,44 @@
 #include <signal.h>
 
 #include <fcntl.h>
-#include <errno.h>
 #include <unistd.h>
 #include <math.h>
+#include <errno.h>
 
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
 #include <sys/types.h>
 
-#define SHM_SIZE 1024
-#define SHM_KEY 1234
+#define SIZE 1024
+#define KEY 1234
 
 int main()
 {
         for(;;)
         {
-                int shmid = shmget(SHM_KEY, SHM_SIZE, IPC_CREAT | 0666);
+                int shmid = shmget(KEY, SIZE, IPC_CREAT | 0666);
                 char *shmaddr = shmat(shmid, (void*)0, 0);
-                int semid = semget(SHM_KEY, 1, IPC_CREAT | 0666);
+                int semid = semget(KEY, 1, IPC_CREAT | 0666);
 
-                struct sembuf sem_st = {0, 0, 0};
-                semop(semid, &sem_st, 1);
+                struct sembuf semcfg = {0,0,0};
+                semop(semid, &semcfg, 1);
 
-                sem_st.sem_op = -1;
-                semop(semid, &sem_st, 1);
-
-                printf("Wiadomosc: %s\n", shmaddr);
+                semcfg.sem_op = -1;
+                semop(semid, &semcfg, 1);
 
                 char ws[100];
-                double wd1;
-                double wd2;
+                double wd1,wd2;
 
-                sscanf(shmaddr, "%s %lf %lf", ws, &wd1, &wd2);
+                sscanf(shmaddr, "%s %lf %lf", ws, &wd1, &wd2);
                 sprintf(shmaddr, "Serwer: %s - %lf", ws, wd1 * wd2);
 
-                sem_st.sem_op = 1;
-                semop(semid, &sem_st, 1);
+                semcfg.sem_op = 1;
+                semop(semid, &semcfg, 1);
 
-                sem_st.sem_op = -1;
-                semop(semid, &sem_st, 1);
-                printf("%s\n", shmaddr);
+                semcfg.sem_op = -1;
+                semop(semid, &semcfg, 1);
+                printf("Wynik: %s\n", shmaddr);
 
                 shmdt(shmaddr);
                 shmctl(shmid, IPC_RMID, NULL);
