f339ad5748c1b51e8b42226cdad814efb54651e9
Daniel2001k <68070950+Daniel2001k@users.noreply.github.com>
2024-04-22 18:35:09 +0200

    Update client.c

--- Bez obslugi bledow/IPC/Message Queuing/client.c
+++ Bez obslugi bledow/IPC/Message Queuing/client.c
@@ -4,67 +4,68 @@
 #include <signal.h>
 
 #include <fcntl.h>
-#include <unistd.h>
-#include <math.h>
 #include <errno.h>
+#include <math.h>
+#include <unistd.h>
 
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
+typedef struct
+{
+        long mtype;
+        double msg;
+} messaged;
+
+typedef struct
+{
+        long mtype;
+        char msg[100];
+} messages;
+
 char path[] = "/tmp/kolejki";
-int queue_id;
+int qid;
 
-typedef struct mystring
-{
-        long mtype;
-        char text[1024];
-} message_s;
-
-typedef struct mydouble
-{
-        long mtype;
-        double number;
-} message_d;
-
-message_s* msg_s;
-message_d* msg_d;
+messaged* msgd;
+messages* msgs;
 
 int main()
 {
-        size_t msg_s_size = sizeof(message_s) - sizeof(long);
-        size_t msg_d_size = sizeof(message_d) - sizeof(long);
+        size_t msgs_size = sizeof(messages) - sizeof(long);
+        size_t msgd_size = sizeof(messaged) - sizeof(long);
+        msgs = malloc(sizeof(messages));
+        msgd = malloc(sizeof(messaged));
+
 
         key_t key = ftok(path, 0);
-        queue_id = msgget(key, 0666);
+        qid = msgget(key, 0666);
 
-        msg_s = malloc(sizeof(message_s));
-        msg_d = malloc(sizeof(message_d));
 
-        msg_s->mtype= 1;
-        sprintf(msg_s->text, "auto");
+        msgs->mtype = 1;
+        msgd->mtype = 1;
 
-        msg_d->mtype = 2;
-        msg_d->number = -7.23;
+        sprintf(msgs->msg, "auto");
+        msgd->msg = -7.23;
 
-        msgsnd(queue_id, (void*)msg_s, msg_s_size, 0);
-        msgsnd(queue_id, (void*)msg_d, msg_d_size, 0);
 
-        msgrcv(queue_id, (void*)msg_s, msg_s_size, 3, 0);
-        msgrcv(queue_id, (void*)msg_d, msg_d_size, 3, 0);
+        msgsnd(qid, (void*)msgs, msgs_size, 0);
+        msgsnd(qid, (void*)msgd, msgd_size, 0);
 
-        printf("Odebralem: %s i %lf\n", msg_s->text, msg_d->number);
 
-        sprintf(msg_s->text, "%s - %lf - Klient", msg_s->text, msg_d->number);
+        msgrcv(qid, (void*)msgs, msgs_size, 2, 0);
+        msgrcv(qid, (void*)msgd, msgd_size, 2, 0);
 
-        msg_s->mtype = 4;
-        msg_d->mtype = 4;
 
-        msgsnd(queue_id, (void*)msg_s, msg_s_size, 0);
+        msgs->mtype = 3;
+        sprintf(msgs->msg, "%s - %lf - Klient", msgs->msg, msgd->msg);
 
-        printf("Wyslalem wiadomosc: %s\n", msg_s->text);
 
-        free(msg_s);
-        free(msg_d);
+        msgsnd(qid, (void*)msgs, msgs_size, 0);
+
+
+        free(msgs);
+        free(msgd);
+
 
         return 0;
 }
