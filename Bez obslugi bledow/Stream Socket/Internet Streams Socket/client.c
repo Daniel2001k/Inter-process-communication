49d9f73d666f4e05c4fdd714eadf9b55b5d7e482
Daniel2001k <68070950+Daniel2001k@users.noreply.github.com>
2024-04-23 23:45:43 +0200

    Update client.c

--- Bez obslugi bledow/Stream Socket/Internet Streams Socket/client.c
+++ Bez obslugi bledow/Stream Socket/Internet Streams Socket/client.c
@@ -1,42 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
 #include <stdlib.h>
+
+#include <unistd.h>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
-
-#include <fcntl.h>
+#include <arpa/inet.h>
 #include <netdb.h>
-#include <unistd.h>
 
 typedef struct sockaddr SockAddr;
 typedef struct sockaddr_in SockAddr_in;
 
+int g;
+
 int main()
 {
-    int gniazdo;
-    socklen_t i;
+    socklen_t i = sizeof(SockAddr_in);
     SockAddr_in adres;
-    char msg[1024];
+    struct hostent *Adres = gethostbyname("127.0.0.1");
     char buf[1024];
 
-    gniazdo = socket(PF_INET, SOCK_STREAM, 0);
-    struct hostent *Adres;
-
-    i = sizeof(SockAddr_in);
+    g = socket(AF_INET, SOCK_STREAM, 0);
     adres.sin_family = AF_INET;
     adres.sin_port = htons(strtol("8080", NULL, 10));
-    Adres = gethostbyname("127.0.0.1");
-    adres.sin_addr.s_addr = *(long *)(Adres->h_addr);
+    adres.sin_addr.s_addr = *(long*)(Adres->h_addr);
 
-    connect(gniazdo, (SockAddr *)&adres, i);
-    printf("Klient: Wpisz wiadomosc do wyslania:\n");
-    fgets(msg, sizeof(msg), stdin);
+    connect(g, (SockAddr*)&adres, i);
+    fgets(buf, sizeof(buf), stdin);
 
-    write(gniazdo, msg, sizeof(msg));
+    write(g, buf, strlen(buf));
 
-    ssize_t liczba_bajtow = read(gniazdo, buf, sizeof(buf));
-    printf("Klient: Odebrano %ld bajtow od serwera, wiadomosc od serwera: %s\n", liczba_bajtow, buf);
-    close(gniazdo);
+    ssize_t liczba_bajtow = read(g, buf, sizeof(buf));
+    printf("%s", buf);
+    close(g);
+
     return 0;
 }
