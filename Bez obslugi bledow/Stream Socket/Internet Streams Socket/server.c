1c326d4da39271bcff77107aef01b5185ea6bfdc
Daniel2001k <68070950+Daniel2001k@users.noreply.github.com>
2024-04-23 23:45:32 +0200

    Update server.c

--- Bez obslugi bledow/Stream Socket/Internet Streams Socket/server.c
+++ Bez obslugi bledow/Stream Socket/Internet Streams Socket/server.c
@@ -3,68 +3,52 @@
 #include <signal.h>
 #include <stdlib.h>
 
+#include <unistd.h>
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#include <unistd.h>
-
 typedef struct sockaddr SockAddr;
 typedef struct sockaddr_in SockAddr_in;
-int gniazdo;
+
+int g, g1;
 
 void al(int i)
 {
-    if (i == SIGTERM || i == SIGINT)
-    {
-        printf("Serwer: Koniec pracy serwera.\n");
-        close(gniazdo);
-        exit(0);
-    }
+    printf("Koniec pracy serwera\n");
+    close(g);
+    exit(0);
 }
 
 int main()
 {
-    int gniazdo1;
     socklen_t i = sizeof(SockAddr_in);
     SockAddr_in adres, adres_k;
 
     signal(SIGTERM, al);
     signal(SIGINT, al);
-    sigignore(SIGCHLD);
 
-    gniazdo = socket(PF_INET, SOCK_STREAM, 0);
-
+    g = socket(AF_INET, SOCK_STREAM, 0);
+    bzero((char *)&adres, sizeof(SockAddr_in));
     adres.sin_family = AF_INET;
     adres.sin_port = htons(strtol("8080", NULL, 10));
     adres.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    bind(gniazdo, (SockAddr *)&adres, i);
-    getsockname(gniazdo, (SockAddr *)&adres, &i);
-    printf("Serwer: Nr portu : %d\n", ntohs(adres.sin_port));
-    listen(gniazdo, 5);
+    bind(g, (SockAddr*)&adres, i);
+    getsockname(g, (SockAddr*)&adres, &i);
+    listen(g, 5);
 
     for (;;)
     {
-        gniazdo1 = accept(gniazdo, (SockAddr *)&adres_k, &i);
-        ssize_t liczba_bajtow;
-        char buf[1024];
+        g1 = accept(g, (SockAddr*)&adres_k, &i);
+        char message[1024];
+        ssize_t liczba_bajtow = read(g1, message, sizeof(message));
+        message[liczba_bajtow] = '\0';
+        printf("%s\n", message);
+        //memset(message, 0, sizeof(message));
+        write(g1, message, strlen(message));
 
-        printf("Serwer: Polaczenie od %s z portu %d.\n", inet_ntoa(adres_k.sin_addr), ntohs(adres_k.sin_port));
-
-        liczba_bajtow = read(gniazdo1, buf, sizeof(buf));
-
-        buf[liczba_bajtow] = '\0';
-        printf("Serwer: Przeczytalem %ld bajtow od klienta, wiadomosc od klienta : %s.\n", liczba_bajtow, buf);
-
-        double temp = atof(buf) * atof(buf);
-        sprintf(buf, "%f", temp);
-
-        strcat(buf, " to jest z serwera");
-
-        write(gniazdo1, buf, strlen(buf));
-
-        close(gniazdo1);
+        close(g1);
     }
-    return 0;
 }
