51c9e7c4d513dc63247318fa48df4847e77e392f
Daniel2001k <68070950+Daniel2001k@users.noreply.github.com>
2024-04-25 15:25:15 +0200

    Update server.c

--- Bez obslugi bledow/Stream Socket/Internet Streams Socket/server.c
+++ Bez obslugi bledow/Stream Socket/Internet Streams Socket/server.c
@@ -8,6 +8,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 
 typedef struct sockaddr SockAddr;
 typedef struct sockaddr_in SockAddr_in;
@@ -30,24 +31,35 @@ int main()
     signal(SIGINT, al);
 
     g = socket(AF_INET, SOCK_STREAM, 0);
-    bzero((char *)&adres, sizeof(SockAddr_in));
+    memset(&adres, 0, sizeof(adres));
     adres.sin_family = AF_INET;
-    adres.sin_port = htons(strtol("8080", NULL, 10));
+    adres.sin_port = htons(8080);
     adres.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    bind(g, (SockAddr*)&adres, i);
-    getsockname(g, (SockAddr*)&adres, &i);
+
+    bind(g, (SockAddr *)&adres, i);
+    getsockname(g, (SockAddr *)&adres, &i);
     listen(g, 5);
 
     for (;;)
     {
-        g1 = accept(g, (SockAddr*)&adres_k, &i);
+        g1 = accept(g, (SockAddr *)&adres_k, &i);
         char message[1024];
         ssize_t liczba_bajtow = read(g1, message, sizeof(message));
-        message[liczba_bajtow] = '\0';
-        printf("%s\n", message);
-        //memset(message, 0, sizeof(message));
-        write(g1, message, strlen(message));
+
+        double temp;
+        sscanf(message, "%lf", &temp);
+        printf("Otrzymano: %lf\n", temp);
+
+        memset(message, 0, sizeof(message));
+        sprintf(message, "To jest z serwera - %lf", temp * temp);
+        printf("Wyslano: %s\n", message);
+
+        write(g1, message, sizeof(message));
+
+        liczba_bajtow = read(g1, message, sizeof(message));
+
+        printf("Wynik: %s\n", message);
 
         close(g1);
     }
