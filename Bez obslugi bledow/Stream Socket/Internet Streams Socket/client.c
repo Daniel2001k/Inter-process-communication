4f0efa78dbc7ec4db76fc8fba8e2c418028c16e6
Daniel2001k <68070950+Daniel2001k@users.noreply.github.com>
2024-04-25 15:31:53 +0200

    Update client.c

--- Bez obslugi bledow/Stream Socket/Internet Streams Socket/client.c
+++ Bez obslugi bledow/Stream Socket/Internet Streams Socket/client.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-#include <signal.h>
 #include <stdlib.h>
 
-#include <unistd.h>
-
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <netdb.h>
+
+#include <unistd.h>
 
 typedef struct sockaddr SockAddr;
 typedef struct sockaddr_in SockAddr_in;
@@ -23,17 +22,30 @@ int main()
     char buf[1024];
 
     g = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&adres, 0, sizeof(adres));
+
     adres.sin_family = AF_INET;
-    adres.sin_port = htons(strtol("8080", NULL, 10));
+    adres.sin_port = htons(8080);
     adres.sin_addr.s_addr = *(long*)(Adres->h_addr);
 
-    connect(g, (SockAddr*)&adres, i);
+    connect(g, (SockAddr *)&adres, i);
     fgets(buf, sizeof(buf), stdin);
 
     write(g, buf, strlen(buf));
 
     ssize_t liczba_bajtow = read(g, buf, sizeof(buf));
-    printf("%s", buf);
+    buf[liczba_bajtow] = '\0';
+
+    char temp[1024];
+    sprintf(temp, "To jest z klienta - %s", buf);
+
+    memset(buf, 0, sizeof(buf));
+
+    sprintf(buf, "%s", temp);
+    printf("%s\n", buf);
+
+    write(g, buf, sizeof(buf));
+
     close(g);
 
     return 0;
