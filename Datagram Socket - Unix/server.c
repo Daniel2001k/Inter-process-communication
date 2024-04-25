b12b0b4f3872b77b43c20dcc4b7e07b23610914c
Daniel2001k <68070950+Daniel2001k@users.noreply.github.com>
2024-04-25 20:47:03 +0200

    Update server.c

--- Datagram Socket - Unix/server.c
+++ Datagram Socket - Unix/server.c
@@ -17,13 +17,18 @@ int main()
     struct sockaddr_un server_addr, client_addr;
     int num1, num2, result;
     socklen_t client_len = sizeof(client_addr);
+    
     server_fd = socket(AF_UNIX, SOCK_DGRAM, 0));
     memset(&server_addr, 0, sizeof(server_addr));
+    
     server_addr.sun_family = AF_UNIX;
     strcpy(server_addr.sun_path, SER_PATH);
     unlink(SER_PATH);
+    
     bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    
     char buffer[256];
+    
     while (1)
     {
         recvfrom(server_fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_addr, &client_len)l
